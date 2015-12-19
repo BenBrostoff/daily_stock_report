801012774a0a50c7d4977947bbc1487a2f6656b7
Ben Brostoff <ben.brostoff@gmail.com>
2015-12-19 13:13:12 -0500

    Add day change

--- stocks.py
+++ stocks.py
@@ -22,6 +22,7 @@ class Stock():
         self.articles = []
         self.ticker = ticker
         self.price = share.get_price()
+        self.day_change = share.get_percent_change()
         self.mcap = share.get_market_cap()
         self.year_low = share.get_year_low()
         self.year_high = share.get_year_high()
@@ -50,9 +51,19 @@ class Stock():
             print 'Error converting articles'
 
     def convert_to_html(self):
-        base = "<strong>%s</strong> <br>CAP: %s \
-                <br>PRICE: %s <br>YLOW: %s (%s) <br>YHIGH: %s (%s)" % (
+        def style(change):
+            if '+' in change:
+                return 'green'
+            else:
+                return 'red'
+
+        base = "<strong>%s</strong> \
+                <br>CAP: %s \
+                <br>PRICE: %s <span style='color:%s'>(%s)</span> \
+                <br>YLOW: %s (%s) <br>YHIGH: %s (%s)" % (
             self.ticker, self.mcap, self.price,
+            style(self.day_change),
+            self.day_change,
             self.year_low, self.off_low,
             self.year_high, self.off_high)
 
